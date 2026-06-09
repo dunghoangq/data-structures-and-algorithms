8bcdc807bc4411758bf2cf2cea531588c801e6b1
dungdz <dunghoangq@gmail.com>
2026-06-09 21:08:23 +0700

    Leetcode > Binary search > problem 268.

---
+++ leetcode/binary_search/268_missing_number/missing_number.cpp
@@ -0,0 +1,54 @@
+/*
+MISSING NUMBER
+
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+e.g.1,
+
+Input: nums = [3,0,1]
+Output: 2
+Explanation:
+n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+
+e.g.2,
+
+Input: nums = [0,1]
+Output: 2
+Explanation:
+n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+
+e.g.3,
+
+Input: nums = [9,6,4,2,3,5,7,0,1]
+Output: 8
+Explanation:
+n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+
+-----------------------------------------
+
+ALGORITHM
+
+MISSING_NUMBER(nums)
+    n = nums.length
+    sum_n = n*(n+1)/2
+    sum_nums = sum(nums)
+
+    return sum_n - sum_nums
+
+COMPLEXITY
+
+- Time: O(n)
+- Space: O(1)
+
+*/
+
+#include <iostream>
+#include <vector>
+#include <numeric>
+
+int missing_number(std::vector<int>& nums) {
+    int n = nums.size();
+    int sum_n = n * (n + 1)/2;
+    int sum_nums = std::accumulate(nums.begin(), nums.end(), 0);
+    return sum_n - sum_nums;
+}
